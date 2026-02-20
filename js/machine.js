73bc409ce97bd0d6bfb48c7ab38d335c515fecf1
AfazurRahman <afazurr8@gmail.com>
2026-02-20 22:40:57 +0600

    fully functioned payoo app

--- js/machine.js
+++ js/machine.js
@@ -14,3 +14,25 @@ function inputValue(id){
     const value=input.value;
     return value;
 }
+
+function selection(id){
+    const select=document.getElementById(id);
+    
+    const addMoney= document.getElementById('add-money-form');
+    addMoney.classList.add("hidden");
+    const cashOut= document.getElementById('cashout-form');
+    cashOut.classList.add("hidden");
+    const sendMoney= document.getElementById('send-money-form');
+    sendMoney.classList.add("hidden");
+    const getBonus= document.getElementById('get-bonus-form');
+    getBonus.classList.add("hidden");
+    const payBill= document.getElementById('pay-bill-form');
+    payBill.classList.add("hidden");
+    const trasactions= document.getElementById('history-container');
+    trasactions.classList.add("hidden");
+
+    select.classList.remove("hidden");
+
+
+    
+}
